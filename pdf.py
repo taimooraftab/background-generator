fa29ce970d311b2d3521979d4ad631e74c6b24a7
Salman Aftab <taimooraftab@ymail.com>
2022-09-27 22:35:08 +0500

    made a change

--- pdf.py
+++ pdf.py
@@ -14,7 +14,7 @@ def pdf_combiner(pdf_list):
 pdf_combiner(inputs)
 
 
-# with open('dummmmy.pdf','rb') as file:
+# with open('yummy.pdf','rb') as file:
 # 	print(file)
 # 	reader = PyPDF2.PdfFileReader(file)
 # 	print(reader.numPages)
